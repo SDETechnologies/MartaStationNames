bda5976fc5059f8d6ff829a097fdac7044c57ce4
Eliot Solbrig <eliot.solbrig@gmail.com>
2024-06-14 12:21:44 -0400

    Test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.4
 
 require (
 	github.com/go-rod/rod v0.116.0 // indirect
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/ysmood/fetchup v0.2.3 // indirect
@@ -11,4 +12,7 @@ require (
 	github.com/ysmood/got v0.34.1 // indirect
 	github.com/ysmood/gson v0.7.3 // indirect
 	github.com/ysmood/leakless v0.8.0 // indirect
+	go.opencensus.io v0.22.3 // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	googlemaps.github.io/maps v1.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    // "io"
-    "github.com/go-rod/rod"
-    // "github.com/go-rod/rod/lib/launcher"
-    "github.com/joho/godotenv"
-    // "github.com/JustinBeckwith/go-yelp"
+	"fmt"
+	"net/http"
+    "os"
+	// "io"
+	"github.com/go-rod/rod"
+	// "github.com/go-rod/rod/lib/launcher"
+	"github.com/joho/godotenv"
+	// "github.com/JustinBeckwith/go-yelp"
 	"github.com/gorilla/mux"
-    // "encoding/json"
+	// "encoding/json"
+	"googlemaps.github.io/maps"
+    "context"
 )
 
 type Station struct {
@@ -21,32 +24,81 @@ type Station struct {
 type ScrapeStationResponse struct {
     Name string `json:"name"`
     Date string `json:"date"`
-    Review string `json:review"`
+    Reviews string `json:review"`
+}
+
+type Review struct {
+    Text string
 }
 
 
-// func scrapeStationNames (w http.ResponseWriter, r *http.Request) {
-func scrapeStationNames () ([]Station, error) {
-    stations := []Station{}
-    browser := rod.New().MustConnect()
-    page := *browser.MustPage("https://www.itsmarta.com/train-stations-and-schedules.aspx").MustWaitLoad()
-    stationElementsDiv := page.MustElement("stations__items isotope")
-    fmt.Println("stationElementsDiv: ", stationElementsDiv)
-    stationElements := stationElementsDiv.MustElements("stations__item")
-    fmt.Println("stationElements: ", stationElements)
-
-    for _,stationElement := range stationElements {
-        fmt.Println("stationElement: ", stationElement)
-        station := Station{}
-        fmt.Println("station: ", station)
-        stationName := stationElement.MustElement("stations__item-name").MustText()
-        station.Name = stationName
-        stations = append(stations, station)
-    } 
-    return stations, nil
+func getReviewFromStationName(stationName string) string {
+    nameForAPI := stationName + " Indian Creek MARTA Station"
+    return nameForAPI
 }
 
-func scrapeStationNames2 () ([]Station, error) {
+
+func getStationReview(ctx context.Context, stationName string) ([]string,error) {
+
+    reviews := []string{} 
+
+    c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("GOOGLE_MAPS_API_KEY")))
+    if err != nil {
+        return reviews, fmt.Errorf("Error querying google api: %s", err)
+    }
+
+    request := maps.FindPlaceFromTextRequest {
+        Input: stationName,
+        InputType: "textquery",
+    }
+    // fmt.Println("request: ", request)
+
+    response,err := c.FindPlaceFromText(ctx, &request)
+    if err != nil {
+        return reviews, fmt.Errorf("Error querying google api: %s", err)
+    }
+    // fmt.Println("response: ", response.PlaceID)
+    // fmt.Println("response: ", response.Candidates)
+
+    fmt.Println("\n----------------------------------------------------------------\n")
+
+
+    placeID := response.Candidates[0].PlaceID
+    fmt.Println("placeID: ", placeID)
+
+    placeDetailsRequest := maps.PlaceDetailsRequest{
+        PlaceID: string(placeID),
+        Language: "en",
+    }
+    fmt.Println("placeDetailsRequest: ", placeDetailsRequest)
+
+    return reviews,  nil
+}
+
+
+// // func scrapeStationNames (w http.ResponseWriter, r *http.Request) {
+// func scrapeStationNames () ([]Station, error) {
+//     stations := []Station{}
+//     browser := rod.New().MustConnect()
+//     page := *browser.MustPage("https://www.itsmarta.com/train-stations-and-schedules.aspx").MustWaitLoad()
+//     stationElementsDiv := page.MustElement("stations__items isotope")
+//     fmt.Println("stationElementsDiv: ", stationElementsDiv)
+//     stationElements := stationElementsDiv.MustElements("stations__item")
+//     fmt.Println("stationElements: ", stationElements)
+//
+//     for _,stationElement := range stationElements {
+//         fmt.Println("stationElement: ", stationElement)
+//         station := Station{}
+//         fmt.Println("station: ", station)
+//         stationName := stationElement.MustElem
+//         ent("stations__item-name").MustText()
+//         station.Name = stationName
+//         stations = append(stations, station)
+//     } 
+//     return stations, nil
+// }
+
+func scrapeStationNames2 (ctx context.Context) ([]Station, error) {
     stations := []Station{}
     browser := *rod.New().MustConnect()
     page := browser.MustPage("https://www.itsmarta.com/train-stations-and-schedules.aspx").MustWaitLoad()
@@ -57,7 +109,16 @@ func scrapeStationNames2 () ([]Station, error) {
         station := Station{}
         fmt.Println("station: ", station)
         stationName := stationElement.MustText()
+        // stationReview := getStationReview(ctx, stationName) 
+        fmt.Println("stationName: ", stationName)
+
+        stationReview,err := getStationReview(context.Background(), stationName)
+        fmt.Println("err: ", err)
+        fmt.Println("stationReview: ", stationReview)
+
         station.Name = stationName
+        station.Review = stationReview
+
         stations = append(stations, station)
     } 
     return stations, nil
@@ -90,7 +151,7 @@ func main(){
     // err = json.Unmarshal(data, &testResponse)
     // fmt.Println("testResponse: ", testResponse)
 
-    stationNames,err := scrapeStationNames2()
+    stationNames,err := scrapeStationNames2(context.Background())
         if err != nil {
             panic(fmt.Errorf("Error scaping staion names in main.go: %s", err))
         }
